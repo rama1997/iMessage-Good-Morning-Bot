abcf4c8ace810ade88f5524ef89d3f06726e5a43
rama1997 <maraymond@yahoo.com>
2024-10-29 12:18:51 -0700

    Update config.py with sportsdataio api key field

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ PATH_TO_APPLESCRIPT = "Enter path to send_message.applescript"
 
 OPENWEATHERMAP_API_KEY = "Enter openweathermap API Key"
 
+SPORTSDATA_API_KEY = "Enter sportsdata.io API Key"
+
 REDDIT_API_ID = "Enter Reddit API ID"
 REDDIT_API_SECRET = "Enter Reddit API Secret"
 REDDIT_USER = "Enter Reddit account username"
@@ -20,6 +22,14 @@ SUBREDDITS = [
     "rarepuppers",
 ]
 
+RAIN_ALERT_MESSAGE = "It is projected to rain today. Stay dry and bring an umbrella. Drive slow and drive safe."
+
+HOLIDAY_MESSAGE = "HAPPY {HOLIDAY}!! 🎉"
+
+DOG_PIC_MESSAGE = (
+    "Here's a daily dog picture to start your day off 🤗 \n\nPost Title: {TITLE} \n{URL}"
+)
+
 MONTH = get_month()
 DAY = get_day()
 DATE = get_date()
@@ -29,16 +39,8 @@ with open("quotes.txt", "r") as file:
 
 MOTIVATION_QUOTE = random.choice(quotes)
 
-RAIN_ALERT_MESSAGE = "It is projected to rain today. Stay dry and bring an umbrella. Drive slow and drive safe."
-
-HOLIDAY_MESSAGE = "HAPPY {HOLIDAY}!! 🎉"
-
-DOG_PIC_MESSAGE = (
-    "Here's a daily dog picture to start your day off 🤗 \n\nPost Title: {TITLE} \n{URL}"
-)
-
 MORNING_GREETING = "Good Morning {NAME} ☀️☀️. Today is {DAY}, {MONTH} {DATE}."
 
-CLOSING_MESSAGE = f"{MOTIVATION_QUOTE} \n Have a good rest of your day.".replace(
+CLOSING_MESSAGE = f"Have a good rest of your day.".replace(
     "'", ""
 )
